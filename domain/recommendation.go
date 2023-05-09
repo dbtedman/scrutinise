fcad16b0338e388003ebdd0f242c585449dc8cee
Daniel Tedman <dbtedman@gmail.com>
2023-05-09 21:06:30 +1000

    #7 Start fresh from the cli

--- domain/recommendation.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-// A Recommendation indicates an action that should be taken in response to an
-// Observation.
-type Recommendation struct {
-}
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+const errorResult = 1
 
 func main() {
 	fmt.Println("scrutinise")
+
+	signalsCh := make(chan os.Signal, 1)
+	resultCh := make(chan int)
+	var result int
+
+	// listen to os signals
+	signal.Notify(signalsCh, os.Interrupt, syscall.SIGTERM)
+
+	defer func() {
+		// cleanup any resources now, then exit
+		os.Exit(result)
+	}()
+
+	go func() {
+		err := errors.New("no command is defined yet")
+
+		if err != nil {
+			fmt.Println(err)
+			resultCh <- errorResult
+		}
+	}()
+
+	select {
+	case <-signalsCh:
+	case result = <-resultCh:
+	}
 }
