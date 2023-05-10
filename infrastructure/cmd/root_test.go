7c972e8ee63736e6ff0046c497606613fd96f8be
Daniel Tedman <dbtedman@gmail.com>
2023-05-10 21:18:41 +1000

    Fix race condition

--- infrastructure/cmd/root_test.go
+++ infrastructure/cmd/root_test.go
@@ -11,22 +11,23 @@ import (
 func TestRootCommand(t *testing.T) {
 	// given
 	resultCh := make(chan int)
+	errorCh := make(chan error)
 	var errConsole bytes.Buffer
 	var outConsole bytes.Buffer
-	var rootCommandExecuteError error
 	command := cmd.RootCommand(&resultCh)
 	command.SetErr(&errConsole)
 	command.SetOut(&outConsole)
 
 	// when
 	go func() {
-		rootCommandExecuteError = command.Execute()
+		errorCh <- command.Execute()
 	}()
-	rootCommandResult := <-resultCh
+	result := <-resultCh
+	err := <-errorCh
 
 	// then
-	assert.Equal(t, cmd.SuccessResult, rootCommandResult)
-	assert.Nil(t, rootCommandExecuteError)
+	assert.Equal(t, cmd.SuccessResult, result)
+	assert.Nil(t, err)
 	assert.Equal(t, "", errConsole.String())
 	assert.Contains(t, outConsole.String(), "Tool to scrutinise website development security")
 	assert.Contains(t, outConsole.String(), "-h, --help   help for scrutinise")
--- infrastructure/cmd/version_test.go
+++ infrastructure/cmd/version_test.go
@@ -11,22 +11,23 @@ import (
 func TestVersionCommand(t *testing.T) {
 	// given
 	resultCh := make(chan int)
+	errorCh := make(chan error)
 	var errConsole bytes.Buffer
 	var outConsole bytes.Buffer
-	var versionCommandExecuteError error
 	command := cmd.VersionCommand(&resultCh)
 	command.SetErr(&errConsole)
 	command.SetOut(&outConsole)
 
 	// when
 	go func() {
-		versionCommandExecuteError = command.Execute()
+		errorCh <- command.Execute()
 	}()
-	versionCommandResult := <-resultCh
+	result := <-resultCh
+	err := <-errorCh
 
 	// then
-	assert.Equal(t, cmd.SuccessResult, versionCommandResult)
-	assert.Nil(t, versionCommandExecuteError)
+	assert.Equal(t, cmd.SuccessResult, result)
+	assert.Nil(t, err)
 	assert.Equal(t, "", errConsole.String())
 	assert.Contains(t, outConsole.String(), "scrutinise version: latest")
 	assert.Contains(t, outConsole.String(), "commit: n/a")
